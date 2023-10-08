52d70ed9b09b691ef17b3b6d421e0a031e80366f
Lucasagus285 <lucasagus285@gmail.com>
2023-10-08 16:13:55 -0300

    Selector de niveles funciona

--- Scripts/SelectorNivel.gd
+++ Scripts/SelectorNivel.gd
@@ -1,7 +1,40 @@
 extends Node2D
 
+func _on_botonSalir_pressed():
+	get_tree().quit()
+
+
 func _on_boton1_pressed():
 	get_tree().change_scene("res://Scenes/Nivel1.tscn")
 
-func _on_botonSalir_pressed():
-	get_tree().quit()
+
+func _on_boton2_pressed():
+	get_tree().change_scene("res://Scenes/Nivel2.tscn")
+
+
+func _on_boton3_pressed():
+	get_tree().change_scene("res://Scenes/Nivel3.tscn")
+
+
+func _on_boton4_pressed():
+	get_tree().change_scene("res://Scenes/Nivel4.tscn")
+
+
+func _on_boton5_pressed():
+	get_tree().change_scene("res://Scenes/Nivel5.tscn")
+
+
+func _on_boton6_pressed():
+	get_tree().change_scene("res://Scenes/Nivel6.tscn")
+
+
+func _on_boton7_pressed():
+	get_tree().change_scene("res://Scenes/Nivel7.tscn")
+
+
+func _on_boton8_pressed():
+	get_tree().change_scene("res://Scenes/Nivel8.tscn")
+
+
+func _on_boton10_pressed():
+	get_tree().change_scene("res://Scenes/Nivel10.tscn")
